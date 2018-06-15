c3e23d250dca7d26b34d50c908621fc9f7be4726
Toeat <924198020@qq.com>
2018-06-15 08:24:23 +0800

    new

---
+++ 若田农业/js/index.js
@@ -0,0 +1,38 @@
+$('.slick1').slick({
+	autoplay:true,
+	arrows:false,
+	fade:true,
+	autoplaySpeed:3000,
+	speed: 600,
+	cssEase: 'linear',
+	pauseOnHover:true,
+//	draggable:false,
+});
+
+$('.slick2').slick({
+//	autoplay:true,
+	arrows:false,
+	fade:true,
+	autoplaySpeed:3000,
+	speed: 600,
+	cssEase: 'linear',
+	pauseOnHover:true,
+	asNavFor: '.slick3',
+//	draggable:false,
+});
+
+$('.slick3').slick({
+  	slidesToShow: 3,
+  	arrows:false,
+  	slidesToScroll:1,
+  	asNavFor: '.slick2',
+  	focusOnSelect: true,
+});
+
+$('.base .btn-l').on('click',function(){
+	$('.slick3').slick('slickPrev');
+});
+
+$('.base .btn-r').on('click',function(){
+	$('.slick3').slick('slickNext');
+});
