6e0b9275393d03c2a6f9c3d93808984f49da0edf
Toeat <924198020@qq.com>
2018-06-24 19:07:26 +0800

    new

--- 若田mobile/js/index.js
+++ 若田mobile/js/index.js
@@ -20,3 +20,30 @@ var mySwiper3 = new Swiper('.swiper3', {
 	spaceBetween : 6,
 	autoplay:false,
 });
+
+var mySwiper4 = new Swiper('.swiper4', {
+	autoplay : 4000,
+	speed:800,
+	autoplay:{
+		disableOnInteraction: false,
+	},
+});
+
+var mySwiper5 = new Swiper('.swiper5', {
+	autoplay : 4000,
+	speed:800,
+	spaceBetween : 6,
+	autoplay:{
+		disableOnInteraction: false,
+	},
+});
+
+var mySwiper6 = new Swiper('.swiper6', {
+	spaceBetween : 10,
+	autoplay : false,	
+});
+
+var mySwiper6 = new Swiper('.swiper7', {
+	autoplay : false,
+	spaceBetween : 12,
+});
