475399c228f95d5d775b99aeab3b427dd454a675
特别能吃 <924198020@qq.com>
2018-07-07 11:23:12 +0800

    180707

--- EFEsan/javascript/31-33小卖家/js/index.js
+++ EFEsan/javascript/31-33小卖家/js/index.js
@@ -3,9 +3,12 @@
 	var oSelettwo = document.getElementById('region-select-two');
 	var oRegion = document.getElementById('region-radio-wrapper');
 	var oProduct = document.getElementById('product-radio-wrapper');
+	var oSeve = document.getElementById('seve');
 	var oCont = document.getElementById('table-wrapper');
 	var Colorarr = ['#609','#666FF','#00C','#93C','#669','#606','#90F','#336','#06C'];	
 	var Colorindex = 0;
+	var nOver = true;
+	var nPrevnum = 0;
 	//选择input的value添加到数组
 	function fnSeletdate(arr){
 		var oDate = [];					
@@ -17,9 +20,18 @@
 		return oDate;
 	}
 	
+	//创建一个input
+	function fnCreinput(val){
+		var oEle = document.createElement('input');
+		oEle.style.width = '40px';
+		oEle.type = 'text';
+		oEle.value = val;
+		return oEle;
+	}
+	
 	//添加元素
 	function fnaddtoEle(data,lenr,lenp){
-		oCont.innerHTML = '';
+		oCont.innerHTML = '';		
 		var oTable = document.createElement('table'),
 			oTit = document.createElement('tr'),
 			ar = ['商品','地区'];
@@ -55,7 +67,7 @@
 				
 				if(x === 'product'){
 					var oTd = document.createElement('td');	
-					if(lenr === 1 && lenp > 1 && onOffp){									
+					if(lenr === 1 && lenp > 1 && onOffp){
 						oTd.innerHTML = data[j].region;
 						oTd.rowSpan = lenp + '';
 						onOffp = false;
@@ -116,7 +128,7 @@
 	//元素点击
 	function fnClick(ev){
 		var e = ev || window.event;　　　　
-		var target = e.target || e.srcElement;
+			target = e.target || e.srcElement;
 		
 		if(target.nodeName === 'INPUT'){
 			var len  = target.parentNode.children.length;
@@ -151,36 +163,175 @@
 			
 			fnEvent(oCont,'mouseover',fnMouseover);
 			
-
-			fnAllline(true);
-			fnAllline(false);
-			
-			fnSetdate();	
 		}
+		
+		var aInputo = oRegion.getElementsByTagName('input'),
+			aInputt = oProduct.getElementsByTagName('input'),
+			aSetone = fnSeletdate(aInputo),
+			aSettwo = fnSeletdate(aInputt),
+			ar = fnSetdate();
+		
+		fnaddtoEle(ar,aSetone.length,aSettwo.length);
+		fnAllline(false);
 	}
+	
+	//判断两个a中是否包含b
+    function contains(a,b){
+        return a.contains ? a != b && a.contains(b) :!!(a.compareDocumentPosition(b) & 16);
+    }
+    
+    //获取事件
+    function getRelated(e){
+        var related;
+        var type=e.type.toLowerCase();//这里获取事件名字
+        if(type=='mouseover'){
+            related=e.relatedTarget||e.fromElement;
+        }else if(type='mouseout'){
+            related=e.relatedTarget||e.toElement;
+        }
+        return related; 
+    }
+    
+    //阻止事件冒泡
+    function stopPropagation(e){    
+	    if(e.stopPropagation) { //W3C阻止冒泡方法  
+	        e.stopPropagation();  
+	    } else {  
+	        e.cancelBubble = true; //IE阻止冒泡方法  
+	    }  
+	}  
+
 	
 	//元素移入
 	function fnMouseover(ev){
 		var e = ev || window.event;　　　　
 		var target = e.target || e.srcElement;
-		var aTr = oCont.getElementsByTagName('tr');
 		var arr = [];
-		var asSeletdata = fnSetdate();
+		var nMax = fnReturNmax();
+		var related = getRelated(e);
 		
-		if(target.nodeName === 'TD'){				
+		if(target.nodeName === 'TD' && target != related && !contains(target,related)){
 			var len  = target.parentNode.children.length;
 			var Nodes = target.parentNode.children;
 			
-			for(var i = 1; i < len; i++){
-				if(parseInt(Nodes[i].innerHTML)){
+			for(var i = 0; i < len; i++){
+				if(!isNaN(Nodes[i].innerHTML)){
 					arr.push(parseInt(Nodes[i].innerHTML));
 				}
-			}
-								
+			}			
+			
 			fnColumnar(arr);
-			fnBrokenline(arr,true,asSeletdata,0);
+			fnBrokenline(arr,true,nMax,0);
+			
+			if(!isNaN(target.innerHTML)){
+				target.innerHTML += '<i>编辑<i>';
+			}
+			
+		}
+		
+	}
+	
+	//元素移出
+	function fnMouseout(ev){
+		var e = ev || window.event;　　　　
+		var target = e.target || e.srcElement;
+		var related = getRelated(e);
+		
+		if(target.nodeName === 'TD' && target != related && !contains(target,related)){
+			if(target.children.length < 2){
+				target.removeChild(target.children[0]);
+			}
 		}
 	}
+	
+	fnEvent(oCont,'mouseout',fnMouseout);
+	
+	function fntdClick(ev){
+		var e = ev || window.event;　　　　
+		var target = e.target || e.srcElement;
+		var related = getRelated(e);
+		
+		if(target.nodeName === 'TD'){
+			if(target.children){
+				target.removeChild(target.children[0]);
+			}
+			
+			var aTd = target.parentNode.parentNode.getElementsByTagName('td');			
+			for(var z = 0; z < aTd.length; z++){
+				if(aTd[z].children.length >= 1){
+					for(var j = 0; j < aTd[z].children.length; j++){
+						aTd[z].removeChild(aTd[z].children[j]);
+					}
+					
+					aTd[z].innerHTML = nPrevnum;
+				}
+			}
+			
+			var val = parseInt(target.innerHTML);
+			target.innerHTML = '';
+			var oTxt = document.createElement('input');
+			var oConfirm = document.createElement('input');
+			var oCancel = document.createElement('input');
+			nPrevnum = val;
+			
+			oTxt.style.width = '50px';
+			oTxt.type = 'text';
+			oTxt.value = val;
+			oConfirm.type = 'submit';
+			oConfirm.value = '确定';
+			oCancel.type = 'reset';
+			oCancel.value = '取消';
+			
+			target.appendChild(oTxt);
+			target.appendChild(oConfirm);
+			target.appendChild(oCancel);
+			
+			target.children[0].onkeyup = function(ev){
+				var ev = ev || window.event;
+				if(ev.keyCode === 13){
+					var num = parseInt(target.children[0].value);
+					if(isNaN(num)){
+						alert('请输入数字');
+					}else{
+						for(var i = 0; i < target.children.length; i++){
+							target.removeChild(target.children[i]);
+						}
+						target.innerHTML = num;
+					}
+				}else if(ev.keyCode === 27){
+					for(var i = 0; i < target.children.length; i++){
+						target.removeChild(target.children[i]);
+					}
+					
+					target.innerHTML = val;
+				}
+			}
+			
+			target.children[1].onclick = function(){
+				var num = parseInt(target.children[0].value);
+				if(isNaN(num)){
+					alert('请输入数字');
+				}else{
+					for(var i = 0; i < target.children.length; i++){
+						target.removeChild(target.children[i]);
+					}
+					target.innerHTML = num;
+				}
+			}
+			
+			target.children[2].onclick = function(){
+				for(var i = 0; i < target.children.length; i++){
+					target.removeChild(target.children[i]);
+				}
+				
+				target.innerHTML = val;
+			}
+			
+			stopPropagation(e);
+		}
+	}
+	
+	fnEvent(oCont,'click',fntdClick);
 	
 	//生成元素
 	function fnCreate(ele,ar){
@@ -195,7 +346,7 @@
 				if(x === 'text'){	
 					ele.innerHTML += ar[i][x];
 				}else if(x === 'value'){
-					oInput.value = ar[i][x];
+					oInput.value = ar[i][x]; 
 				}
 			}
 		}
@@ -225,6 +376,7 @@
 		return ar;
 	}
 	
+	//筛选指定数组
 	function fnSetdate(){
 		var ar = [],
 			aInputo = oRegion.getElementsByTagName('input'),
@@ -234,28 +386,105 @@
 		
 		ar = fnAddarr(aSetone,sourceData);
 		ar = fnAddarr(aSettwo,ar);
-	
-		fnaddtoEle(ar,aSetone.length,aSettwo.length);
 		return ar;
 	}
 	
 	//显示选中所有线
 	function fnAllline(tf){
-		var asSeletdata = fnSetdate();
+		var asSeletdata = fnAlladd();
+		var nMax = fnReturNmax();
 		for(var x = 0; x < asSeletdata.length; x++){
-			for(z in asSeletdata[x]){
-				if(z === 'sale'){
-					fnBrokenline(asSeletdata[x][z],tf,asSeletdata,Colorarr[x]);
-					console.log(Colorarr[x]);
-				}				
+			for(var z = 0; z < asSeletdata.length; z++){
+				fnBrokenline(asSeletdata[x],tf,nMax,Colorarr[x]);				
 			}				
 		}		
 	}
 	
-	oCont.onmouseleave = function(){
-		fnAllline(true);
+	//返回表格中所有数字的最大数
+	function fnReturNmax(){
+		if(oCont.children[0]){
+			var tab = oCont.children[0];
+			var nMax = 0;
+			for(var i = 1; i < tab.rows.length; i++){
+				for(var x = 0; x < tab.rows[i].cells.length; x++){
+					if(!isNaN(tab.rows[i].cells[x].innerHTML)){
+						if(nMax < parseInt(tab.rows[i].cells[x].innerHTML)){
+							nMax = parseInt(tab.rows[i].cells[x].innerHTML);
+						};
+					}
+				}			
+			}			
+			return nMax;
+		}
+	}
+	
+	//选择表格中的所有数字添加到一个数组
+	function fnAlladd(){
+		if(oCont.children[0]){
+			var tab = oCont.children[0];
+			var arr = [];
+			var ar = [];
+			for(var i = 1; i < tab.rows.length; i++){
+				ar = [];
+				for(var x = 0; x < tab.rows[i].cells.length; x++){
+					if(!isNaN(tab.rows[i].cells[x].innerHTML)){
+						ar.push(parseInt(tab.rows[i].cells[x].innerHTML));
+						
+					}
+				}
+				
+				arr.push(ar);
+			}			
+			return arr;
+		}
+	}
+	var tabledata = null
+	
+	//保存选中数据
+	oSeve.onclick = function(){
+		var aInputo = oRegion.getElementsByTagName('input'),
+			aInputt = oProduct.getElementsByTagName('input'),
+			tabledata = fnSetdate();
+	    var str = JSON.stringify(tabledata);
+	    localStorage.tabledata = str;   
+	    localStorage.lenr = fnSeletdate(aInputo);
+		localStorage.lenp = fnSeletdate(aInputt);
+	}
+	
+	if(localStorage.tabledata){
+		var data = localStorage.tabledata;
+		var lenr = localStorage.lenr.split(',');
+		var lenp = localStorage.lenp.split(',');
+		
+		tabledata = JSON.parse(data);	
+
+		fnaddtoEle(tabledata,lenr.length,lenp.length);
+		fnEvent(oCont,'mouseover',fnMouseover);
 		fnAllline(false);
-	};
+		oCont.onmouseleave = function(){
+			fnAllline(false);
+		};
+	}else{
+		//鼠标离开表格
+		oCont.onmouseleave = function(){
+			fnAllline(true);
+			fnAllline(false);
+		};
+	}
+	
+	//使只有一个单元格处于编辑状态
+	document.onclick = function(){
+		var aTd = oCont.getElementsByTagName('td');			
+		for(var z = 0; z < aTd.length; z++){
+			if(aTd[z].children.length >= 1){
+				for(var j = 0; j < aTd[z].children.length; j++){
+					aTd[z].removeChild(aTd[z].children[j]);
+				}
+				
+				aTd[z].innerHTML = nPrevnum;
+			}
+		}
+	}
 	
 	fnCreate(oRegion, [{
 	    value:"华东",
--- EFEsan/javascript/31-33小卖家/js/line.js
+++ EFEsan/javascript/31-33小卖家/js/line.js
@@ -1,15 +1,13 @@
-var num = 0;
-function fnBrokenline(data,onOff,adata,color){
+function fnBrokenline(data,onOff,nMax,color){
 	var canvas = document.getElementById('Brokenline');
 	//重置canvas
 	if(onOff){
-		canvas.height = canvas.height
+		canvas.height = canvas.height;
 	}  
 		
 	
 	if(canvas.getContext){
 		var ctx = canvas.getContext('2d');
-		var nMax = 0;
 		var len = data.length;
 		var oldxy = [0,325];
 						
@@ -28,23 +26,7 @@ function fnBrokenline(data,onOff,adata,color){
 		ctx.lineTo(500,325);
 		ctx.closePath();
 		ctx.stroke();
-		
-		//获取数据最大值
-
-		for(var x = 0; x < adata.length; x++){
-			for(z in adata[x]){
-				if(z === 'sale'){
-					for(var j = 0; j < adata[x][z].length; j++){
-						if(nMax < adata[x][z][j]){
-							nMax = adata[x][z][j];
-						}
-					}
-				}				
-			}
-			
-		}
-
-		
+				
 		//绘制折线
 		ctx.beginPath(0,0);
 
@@ -58,8 +40,7 @@ function fnBrokenline(data,onOff,adata,color){
 			ctx.arc(oldxy[0],oldxy[1],2,0,Math.PI*2 ,false);
 		}
 		ctx.closePath();
-		ctx.stroke();
-		
+		ctx.stroke();		
 	}		
 }
 
